5afdbfa575b9a3ae5090fdef77484845b80bf1ec
Marisixande <mariaclaramacedosouza71@gmail.com>
2025-10-07 17:11:29 -0300

    introduçao java script 2

---
+++ js/intjsdois.js
@@ -0,0 +1,36 @@
+// //String
+// let nome = "Alex" ;
+
+// //Number(Integer)
+// let idade = 16 ;
+
+// //Boolean 
+// let estudante= true ;
+
+// //Array
+// let frutas = ["maçã" , "banana" , "laranja"] ;
+
+// //Object 
+// let pessoa = {
+//     nome: "Alex" ,
+//     idade: 30 ,
+//     altura:1.80
+// } ;
+
+// //Null
+// let valorNulo = null ;
+
+// //Undefined
+// let valorIndefinido ;
+
+// //Symbol
+// let simbolo = Symbol("chave") ;
+
+// //Function
+// function somar(a , b) {
+//     return a + b ;
+// }
+
+//atividade slide 23
+
+
---
+++ js/script.js
@@ -0,0 +1,7 @@
+// //slide 24
+// let nome = window.prompt("Qual o seu nome ?")
+
+// alert( `Olá ${nome}!, sejá bem vindo ao meu site! `)
+
+
+alert('Eu estou estudando s')
