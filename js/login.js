5b9e257a634a12e020c52c7129ea7f3a1aba9c84
Marisixande <mariaclaramacedosouza71@gmail.com>
2025-10-09 17:16:59 -0300

    ACABOUUUU

---
+++ js/login.js
@@ -0,0 +1,12 @@
+function mudarCor() {
+    const quadrado = document.getElementById(id = "quadrado")
+    const cor = document.getElementById(id = "cor").value
+}
+
+if (cor === "azul") {
+    quadrado.style.backgroundColor ="Blue" 
+} else if (color === "verde"){
+    quadrado.style.backgroundColor = "Green"
+} else if (color === "preto"){
+    quadrado.style.backgroundColor = "Black"
+}
